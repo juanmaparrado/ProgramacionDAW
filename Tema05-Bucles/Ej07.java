f47df9d4040eb54d1b547029a172e966e9402e05
juanmaparrado <juanmanuel.parrado.fp@iescampanillas.com>
2021-10-27 11:14:05 +0200

    actualizacion

--- Tema05-Bucles/Ej07.java
+++ Tema05-Bucles/Ej07.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
 public class Ej07 {
 public static void main(String[] args) {
-
+ int NumSecreto=1234 ;
+ int contador = 0;
+ int NumIntroducido=0;
+ boolean    Acierto = false ; 
+        Scanner s = new Scanner(System.in);
+        while (contador<4 && Acierto == false) {
+           System.out.println("Introduce el numero de la combinacion");
+        NumIntroducido = s.nextInt();
+        if (NumIntroducido == NumSecreto) {
+             Acierto = true;   
+        }  
+        else{
+                System.out.println("Esa no es la combinacion");
         }
-    
+        contador++;    
+        }
+        if (Acierto == true) {
+                System.out.println("FELICIDADES ABRISTE LA CAJA FUERTE");     
+        }
+        s.close();
 }
-    
+}
--- Tema05-Bucles/Ej08.java
+++ Tema05-Bucles/Ej08.java
@@ -1,5 +1,20 @@
+import java.nio.channels.NetworkChannel;
+import java.util.Scanner;
 public class Ej08 {
     public static void main(String[] args) {
-        
+        Scanner s = new Scanner(System.in);
+        int numIntroducido = 0;
+        int multiplicador = 0;
+        int contador = 0;
+        int resultado = 0;
+        System.out.println("Introduce el numero de la tabla de multiplicar que deseas ver");
+        numIntroducido = s.nextInt();
+        while (contador < 10) {
+            multiplicador++;
+            resultado = numIntroducido*multiplicador;
+            System.out.println(numIntroducido+" * "+multiplicador+" = "+resultado);
+            contador++;   
+        }
+        s.close();
     }
 }

bb78bc05dfd3ef0e480d733583a1bccd9ae870d1
Juan Manuel Parrado <91953073+juanmaparrado@users.noreply.github.com>
2021-11-11 13:05:28 +0100

    update

--- Tema06-NumerosAleatorios/Ejercicio12.java
+++ Tema06-NumerosAleatorios/Ejercicio12.java
@@ -3,6 +3,10 @@ public class Ejercicio12 {
         /*12. Realiza un programa que llene la pantalla de caracteres aleatorios (a lo Matrix) con el
 * código ascii entre el 32 y el 126. Puedes hacer casting con (char) para convertir un
 * entero en un carácter. */
-
+    char caracter;
+    for (int i = 0; i < 5000; i++) {
+        caracter = (char)((Math.random())*(126-32+1)+32);
+        System.out.print(caracter);
+    }
     }
 }

890f7a5d3647c07fc3bc21c0cb03aa10dc56aff6
Juan Manuel Parrado <91953073+juanmaparrado@users.noreply.github.com>
2021-11-11 09:37:09 +0100

    update

--- Tema06-NumerosAleatorios/Ejercicio9.java
+++ Tema06-NumerosAleatorios/Ejercicio9.java
@@ -7,11 +7,13 @@ public class Ejercicio9 {
     public static void main(String[] args) {
        int cantidad =0;
        int n = 0;
-      while (n != 24); {
-          n = (int)((Math.random())*100);
-            System.out.print(n+" ");
-            cantidad++;
-      } 
-     System.out.println("Se han generado "+cantidad+"numeros.");
+       do {
+           n = (int)((Math.random())*101);
+            if (n % 2 ==0) {
+                 System.out.print(n+" ");
+                 cantidad++;
+            }
+       } while (n != 24);
+     System.out.println("Se han generado "+cantidad+" numeros.");
     }
 }

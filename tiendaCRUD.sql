26e43e1be911f5e8d1323a295a0155cde4dc8e71
Juan Manuel Parrado <91953073+juanmaparrado@users.noreply.github.com>
2022-05-09 09:43:13 +0200

    Add files via upload

--- tiendaCRUD.sql
+++ tiendaCRUD.sql
@@ -20,10 +20,10 @@ USE `tiendaCRUD`;
 
 CREATE TABLE IF NOT EXISTS `producto` (
   `productoID` int(11) NOT NULL,
-  `nombre` varchar(40) COLLATE utf8_spanish2_ci DEFAULT NULL,
-  `proveedor` varchar(100) NOT NULL CHECK proveedor LIKE ('Xiaomi','Apple','Samsung','LG','Realme','Huawei'),
-  `precio` int(11) ,
-  `cantidad` INT(11) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `nombre` varchar(40) COLLATE utf8_spanish2_ci NOT NULL,
+  `proveedor` varchar(100) NOT NULL CHECK ( proveedor IN ('Xiaomi','Apple','Samsung','LG','Realme','Huawei')),
+  `precio` int(11) NOT NULL,
+  `cantidad` INT(11) COLLATE utf8_spanish2_ci NOT NULL,
   PRIMARY KEY (`productoID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
@@ -33,13 +33,13 @@ CREATE TABLE IF NOT EXISTS `producto` (
 
 INSERT INTO `producto` (`productoID`, `nombre`, `proveedor`, `precio`, `cantidad`) VALUES
 (1200, 'Iphone XR', 'Apple', 1000, 46),
-(1200, 'Redmi Note 10', 'Xiaomi', 250, 150),
-(1200, 'Galaxy S10', 'Samsung', 750, 67),
-(1200, 'P40', 'Huawei', 600, 230),
-(1200, 'POPOO 32', 'Realme', 500, 70),
-(1200, 'Galaxy S12', 'Samsung', 1200, 20),
-(1200, 'Iphone 8', 'Apple', 500, 34),
-(1200, 'Mi Lite 10', 'Xiaomi', 250, 150);
+(1201, 'Redmi Note 10', 'Xiaomi', 250, 150),
+(1202, 'Galaxy S10', 'Samsung', 750, 67),
+(1203, 'P40', 'Huawei', 600, 230),
+(1204, 'POPOO 32', 'Realme', 500, 70),
+(1205, 'Galaxy S12', 'Samsung', 1200, 20),
+(1206, 'Iphone 8', 'Apple', 500, 34),
+(1207, 'Mi Lite 10', 'Xiaomi', 250, 150);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
